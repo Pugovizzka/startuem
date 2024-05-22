12df26260fe0c2987be4331fb958dcf09b08dfbc
Pugovizzka <kv.kozlof@gmail.com>
2024-05-22 17:56:39 +0300

    main file for translator

---
+++ main.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include "bst.h"
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ru");
+    Dictionary<std::string> vocabulary;
+    std::string filename = "file.txt";
+    std::ifstream stream(filename);
+    try {
+        if (!stream.is_open()) {
+            throw std::runtime_error("Не удалось открыть файл");
+        }
+    }
+    catch (const std::runtime_error& error) {
+        std::cerr << error.what() << "\n";
+        return 0;
+    }
+    while (!stream.eof()) {
+        std::string word;
+        getline(stream, word);
+        if (word != "") {
+            vocabulary.INSERTWORD(word);
+        }
+        std::string translation;
+        getline(stream, translation);
+        getline(stream, translation);
+        while (translation != "")
+        {
+            if (translation != "Нет переводов") {
+                vocabulary.ADDTRANSLATION(word, translation);
+            }
+            getline(stream, translation);
+        }
+    }
+    std::string operation;
+    std::cout << "Выберите номер операции:\n1 - записать новое слово\n2 - записать новый перевод\n3 - показать перевод слова\n4 - удалить слово\n5 - показать все слова в словаре\n6 - завершить программу\n";
+    std::cin >> operation;
+    while (operation != "6") {
+        if (operation == "1") {
+            std::string newWord;
+            std::cout << "Введите слово, которое хотите добавить в словарь:\n";
+            std::cin >> newWord;
+            vocabulary.INSERTWORD(newWord);
+        }
+        else if (operation == "2") {
+            std::string word;
+            std::cout << "Введите слово, для которого хотите добавить перевод:\n";
+            std::cin >> word;
+            std::string translation;
+            std::cout << "Введите новый перевод слвоа:\n";
+            std::cin >> translation;
+            vocabulary.ADDTRANSLATION(word, translation);
+        }
+        else if (operation == "3") {
+            std::string word;
+            std::cout << "Введите слово, которое хотите перевести:\n";
+            std::cin >> word;
+            vocabulary.TRANSLATEWORD(word);
+        }
+        else if (operation == "4") {
+            std::string word;
+            std::cout << "Введите слово, которое хотите удалить:\n";
+            std::cin >> word;
+            vocabulary.DELETEWORD(word);
+        }
+        else if (operation == "5") {
+            vocabulary.VIEWALLWORDS();
+        }
+        else {
+            std::cout << "Неверная операция, попробуйте снова!\n";
+        }
+        std::cout << "Введите номер операции:\n";
+        std::cin >> operation;
+    }
+    vocabulary.reloadTheDictionary(filename);
+    return 0;
+}
